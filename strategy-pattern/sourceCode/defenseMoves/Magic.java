afae13b6a303a8f96ae702486b8c09209ba5fee2
Vem Aiensi Marasigan <vem.aiensi@gmail.com>
2024-02-08 18:30:55 +0800

    Update Magic.java

--- strategy-pattern/sourceCode/defenseMoves/Magic.java
+++ strategy-pattern/sourceCode/defenseMoves/Magic.java
@@ -3,6 +3,6 @@ package sourceCode.defenseMoves;
 public class Magic implements DefenseStrat{
     @Override
     public String defend(){
-        return "Using a shield to defend! \n\t";
+        return "Creating a magic barrier for defense!";
     }
 }
